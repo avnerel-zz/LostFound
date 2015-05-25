6a71bd93fb510ca19769205be8b7fa26d6339d11
etayhn <etayhn@users.noreply.github.com>
2015-05-25 21:40:25 +0300

    More done with push notifications. Still need to save the data in local datastore

--- app/src/main/java/com/avner/lostfound/PushNotificationReceiver.java
+++ app/src/main/java/com/avner/lostfound/PushNotificationReceiver.java
@@ -3,8 +3,17 @@ package com.avner.lostfound;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
 
+import com.parse.ParseObject;
 import com.parse.ParsePushBroadcastReceiver;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.Iterator;
+import java.util.Map;
 
 /**
  * Created by avner on 04/05/2015.
@@ -15,13 +24,70 @@ public class PushNotificationReceiver extends ParsePushBroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
 
-        if(((LostFoundApplication)context.getApplicationContext()).isMessagingActivityActive()){
+        if (intent.hasExtra("com.parse.Data")) {
+            String stringData = intent.getStringExtra("com.parse.Data");
 
-            // no need for popup notification. already in messaging session.
-            return;
+            JSONObject jsonData = null;
+            try {
+                jsonData = new JSONObject(stringData);
+                Log.d("PUSH_RECEIVED", "jsonData: " + jsonData);
+
+                if (!handlePushes(context, jsonData)) return;
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
         }
-
         super.onReceive(context, intent);
+    }
+
+    /**
+     * @return false iff the push should show a notification
+     */
+    private boolean handlePushes(Context context, JSONObject jsonData) throws JSONException {
+        String pushType = (String) jsonData.get("pushType");
+        Log.d("PUSH_RECEIVED", "pushType: " + pushType);
+        switch (pushType) {
+            case "ParseMessage":
+                handlePushOfParseMessage(jsonData);
+
+                if (((LostFoundApplication) context.getApplicationContext()).isMessagingActivityActive()) {
+                    // no need for popup notification. already in messaging session.
+                    return false;
+                }
+                break;
+            case "ParseLost":
+                handlePushOfParseLost(jsonData);
+                return false;
+            case "ParseFound":
+                handlePushOfParseFound(jsonData);
+                return false;
+        }
+        return true;
+    }
+
+    private void handlePushOfParseFound(JSONObject jsonData) throws JSONException {
+        String reportedItem = (String) jsonData.get("reportedItem");
+        Log.d("PUSH_FOUND", reportedItem);
+        // TODO: create object from the data, as in handlePushOfParseLost
+    }
+
+    private void handlePushOfParseLost(JSONObject jsonData) throws JSONException {
+        String reportedItem = (String) jsonData.get("reportedItem");
+        Log.d("PUSH_LOST", reportedItem);
+        StringBuilder sb = new StringBuilder("KeySet: [");
+        for (Iterator iterator = new JSONObject(reportedItem).keys(); iterator.hasNext(); ) {
+            sb.append(iterator.next());
+            if (iterator.hasNext())
+                sb.append(",");
+        }
+        Log.d("PUSH_LOST", sb.append("]").toString());
+
+        // TODO: create object from the data
+    }
+
+    private void handlePushOfParseMessage(JSONObject jsonData) {
+        // TODO: Avner, when a message is added the json object will hold: recipientId, itemId, messageText. just like you asked
     }
 
     @Override
